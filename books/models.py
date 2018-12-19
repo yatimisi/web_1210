01da9311d2fd0024dacd96077bb7f0ff1b0b0f29
Yatimisi <yatimisi2012@gmail.com>
2018-12-19 20:51:56 +0800

    2018-12-19

--- books/models.py
+++ books/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=20)
-    price = models.PositiveIntegerField()
-    introduction = models.TextField()
+    name = models.CharField('書名', max_length=20)
+    price = models.PositiveIntegerField('價格')
+    introduction = models.TextField('簡介')
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.id)
